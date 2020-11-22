22105ee0882af59a25d68bd9d2edd9c4ba2e0b20
Amir <amnik_75@yahoo.com>
2020-11-22 11:30:50 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo -e "nameserver 178.22.122.100\nnameserver 185.51.200.2" | sudo tee  /etc/re
 sudo -i -u stack bash << EOF
 git clone https://git.openstack.org/openstack-dev/devstack
 cd devstack
-echo -e '[[local|localrc]]\nADMIN_PASSWORD=PmSrR1399\nDATABASE_PASSWORD=$ADMIN_PASSWORD\nRABBIT_PASSWORD=$ADMIN_PASSWORD\nSERVICE_PASSWORD=$ADMIN_PASSWORD' | sudo tee  local.conf
+echo -e '[[local|localrc]]\nADMIN_PASSWORD=PmSrR1399\nDATABASE_PASSWORD=PmSrR1399\nRABBIT_PASSWORD=PmSrR1399\nSERVICE_PASSWORD=PmSrR1399' | sudo tee  local.conf
 echo -e "enable_plugin networking-odl http://git.openstack.org/openstack/networking-odl $1\nODL_MODE=allinone" | sudo tee -a local.conf
 echo -e "Q_ML2_PLUGIN_MECHANISM_DRIVERS=opendaylight,logger\nODL_GATE_SERVICE_PROVIDER=vpnservice\ndisable_service q-l3" | sudo tee -a local.conf
 echo -e "ML2_L3_PLUGIN=odl-router\nODL_PROVIDER_MAPPINGS={PUBLIC_PHYSICAL_NETWORK}:ens3" | sudo tee -a local.conf
